03187a27b25be1e466caeed18624087344ddad8d
Vadim <chakapega@gmail.com>
2019-05-23 02:55:22 +0300

    Added method for deleting documents and and refactoring code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 const addedTasksContainer = document.querySelector('.added-tasks_container');
 const newTaskForm = document.querySelector('.new-task_form');
 
-db.collection('tasks').get().then(snapshot => {
-  snapshot.docs.forEach(doc => {
-    renderTask(doc);
+window.onload = () => {
+  deletionTask();
+  loadingCurrent();
+};
+
+const loadingCurrent = () => {
+  db.collection('tasks').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+      renderTask(doc);
+    });
   });
-});
+};
+
+const deletionTask = () => {
+  const quantityTasks = document.querySelectorAll('.added-task_container').length;
+
+  for(let i = 0; i < quantityTasks; i++) {
+    document.querySelector('.added-task_container').remove();
+  };
+};
 
 const renderTask = doc => {
   const taskContainer = document.createElement('div');
   taskContainer.classList.add('added-task_container');
+  taskContainer.setAttribute('data-id', doc.id);
 
   const taskSpan = document.createElement('span');
   taskSpan.classList.add('added-task_span');
@@ -24,7 +40,18 @@ const renderTask = doc => {
   taskContainer.appendChild(xSpan);
 
   addedTasksContainer.appendChild(taskContainer);
-}
+
+  xSpan.addEventListener('click', e => {
+    e.stopPropagation();
+
+    deletionTask();
+
+    let id = e.target.parentElement.getAttribute('data-id');
+    db.collection('tasks').doc(id).delete();
+
+    loadingCurrent();
+  });
+};
 
 const addNewTaskButton = document.querySelector('.add-new-task__button');
 addNewTaskButton.addEventListener('click', () => {
@@ -35,9 +62,18 @@ addNewTaskButton.addEventListener('click', () => {
 newTaskForm.addEventListener('submit', e => {
   e.preventDefault();
 
+  deletionTask();
+
   db.collection('tasks').add({
     name: e.srcElement[0].value,
     description: e.srcElement[1].value
   });
 
+  e.srcElement[0].value = '';
+  e.srcElement[1].value = '';
+
+  document.querySelector('.new-task_form').style.display = 'none';
+  addNewTaskButton.style.display = 'inline';
+
+  loadingCurrent();
 });
